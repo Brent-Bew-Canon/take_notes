5991de09ec4a4df8468c26fe3fd710ec3badce58
Brent <124935518+Brent-Bew-Canon@users.noreply.github.com>
2023-05-17 09:42:14 -0700

    updated route for /notes

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ const routes = require("express").Router()
 const notes = require("./notes")
 const path = require("path")
 
-routes.use("/api", notes)
+
 
 routes.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/notes.html"))
 })
+
+routes.use("/api", notes)
 
 module.exports = routes
