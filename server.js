c15d3bb0676171308dc38efea5eafcae7d7febdb
Brent <124935518+Brent-Bew-Canon@users.noreply.github.com>
2023-05-17 09:25:46 -0700

    Fix Port Error

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require("express")
-
 const app = express()
-
 const routes = require("./routes")
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json())
 app.use(express.static("public"))
 app.use(routes)
 
-app.listen(3001, () => {
-    console.log("App running on port 3001...")
+
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}...`)
 })
